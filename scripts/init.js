67eb1dd24f72f8fbb7584468b29f7edc43918bc8
hadihariri <mail@hadihariri.com>
2017-06-26 13:47:01 +0200

    Nearly!

--- scripts/init.js
+++ scripts/init.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-require('./executable-code/executable-fragment.scss')
 const CodeMirror = require('./codemirror/CodeMirror');
 const ExecutableCode = require('./executable-code');
 
