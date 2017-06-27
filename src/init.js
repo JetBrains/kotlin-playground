956f9e2a199bafbac6be2ba0ede65602b83627d1
Paul Everitt <pauleveritt@me.com>
2017-06-27 15:30:41 -0400

    Hmm, don't really need the codemirror.js, just inline into index.js.

--- src/init.js
+++ src/init.js
@@ -1,7 +1,17 @@
 import $ from 'jquery';
-const CodeMirror = require('./codemirror/CodeMirror');
+
 const ExecutableCode = require('./executable-code');
+
+// Bring in codemirror
+require('codemirror/addon/runmode/colorize.js');
+require('codemirror/mode/clike/clike.js');
+require('codemirror/mode/groovy/groovy.js');
+require('codemirror/mode/xml/xml.js');
+require('codemirror/mode/javascript/javascript.js');
+require('codemirror/mode/shell/shell.js');
+
 
 $(document).ready(function () {
     new ExecutableCode('.sample');
+    console.log(99);
 });
