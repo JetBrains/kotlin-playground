d92cf9ff3ebe256405378d94d84402d23607ee1e
zoobestik <kb.chernenko@gmail.com>
2023-08-20 05:19:29 +0200

    test(KTL-1189); reformat by prettier

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,11 @@ import { defineConfig, devices } from '@playwright/test';
 
 dotenv({ path: `.env.local`, override: true });
 
-const isDevMode = Boolean(env.TEST_MODE === 'dev');
+const isDevMode = Boolean(env.TEST_MODE !== 'full');
 
 function getHeadlessMode() {
-    const { TEST_HEADLESS_MODE } = env;
-    return TEST_HEADLESS_MODE ? TEST_HEADLESS_MODE === 'true' : !isDevMode;
+  const { TEST_HEADLESS_MODE } = env;
+  return TEST_HEADLESS_MODE ? TEST_HEADLESS_MODE === 'true' : !isDevMode;
 }
 
 function getProjects() {
@@ -16,51 +16,52 @@ function getProjects() {
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    }
+    },
   ];
 
-  if (!isDevMode) projects = projects.concat(
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  );
+  if (!isDevMode)
+    projects = projects.concat(
+      {
+        name: 'firefox',
+        use: { ...devices['Desktop Firefox'] },
+      },
+      {
+        name: 'webkit',
+        use: { ...devices['Desktop Safari'] },
+      },
+    );
 
   return projects;
 }
 
 export default defineConfig({
-    testDir: './tests',
-    testMatch: /.*\.e2e\.tsx?$/,
+  testDir: './tests',
+  testMatch: /.*\.e2e\.tsx?$/,
 
-    timeout: 60000,
-    forbidOnly: !isDevMode,
-    reporter: 'list',
-    retries: isDevMode ? 0 : 2,
-    fullyParallel: !isDevMode,
+  timeout: 60000,
+  forbidOnly: !isDevMode,
+  reporter: 'list',
+  retries: isDevMode ? 0 : 2,
+  fullyParallel: !isDevMode,
 
-    webServer: {
-        command: 'npm run test:server',
-        port: 8000,
-        reuseExistingServer: isDevMode,
-    },
+  webServer: {
+    command: 'npm run test:server',
+    port: 8000,
+    reuseExistingServer: isDevMode,
+  },
 
-    use: {
-      testIdAttribute: 'data-test',
-      headless: getHeadlessMode(),
-      ignoreHTTPSErrors: true,
-      screenshot: {
+  use: {
+    testIdAttribute: 'data-test',
+    headless: getHeadlessMode(),
+    ignoreHTTPSErrors: true,
+    screenshot: {
       fullPage: true,
       mode: isDevMode ? 'only-on-failure' : 'on',
     },
 
     trace: isDevMode ? 'on-first-retry' : 'on',
-        video: isDevMode ? 'on-first-retry' : 'on',
-    },
+    video: isDevMode ? 'on-first-retry' : 'on',
+  },
 
-    projects: getProjects(),
+  projects: getProjects(),
 });
